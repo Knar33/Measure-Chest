66105ef354ab1858b5e1a35a85ce8e5b3ef69a54
Knar33 <adbyerly@comcast.net>
2020-01-27 23:08:37 -0600

    Read and play audio from SD card

--- Measure-Chest.ino
+++ Measure-Chest.ino
@@ -1,15 +1,24 @@
+#include "SD.h"
+#define SD_ChipSelectPin 10
+#include "TMRpcm.h"
+#include "SPI.h"
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_L3GD20_U.h>
+
+#define SD_ChipSelectPin 10
 
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
 float angle;
 float measurementsPerSecond = 10;
 float rotationalThreshold = .05;
+TMRpcm tmrpcm;
 
 void setup() {
   Serial.begin(9600);
+
   angle = 0;
+  tmrpcm.speakerPin = 9;
   
   gyro.enableAutoRange(true);
   if(!gyro.begin(GYRO_RANGE_2000DPS))
@@ -17,6 +26,13 @@ void setup() {
     Serial.println("Error connecting to gyro");
     while(1);
   }
+
+  if(!SD.begin(SD_ChipSelectPin))
+  {
+    Serial.println("SD fail");
+    while(1);
+  }
+  tmrpcm.setVolume(6);
   delay(1000);
 }
 
@@ -29,6 +45,10 @@ void loop() {
     //In order to ROUGHLY calculate the angle change during this time frame, we divide by our measurementsPerSecond then multiply by 57.2958 (an approximation of degrees per radian)
     //This will result in drift over time, but the angle will be reset everytime the box is closed.
     angle += event.gyro.z / measurementsPerSecond * 57.2958;
+  }
+
+  if (angle > 100 && !tmrpcm.isPlaying()) {
+    tmrpcm.play("opened3.wav");
   }
   
   Serial.print("Z-Rotation: "); Serial.print(event.gyro.z); Serial.print("; Angle: "); Serial.println(angle);
