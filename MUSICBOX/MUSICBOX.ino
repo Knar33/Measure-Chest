399cdfc022acc2c5cee8798bd9a1eeb75206865a
Knar <knarxr@protonmail.com>
2020-02-26 00:30:19 -0600

    Added time signatures

--- MUSICBOX/MUSICBOX.ino
+++ MUSICBOX/MUSICBOX.ino
@@ -14,12 +14,13 @@
 #define zModifier .5
 
 int scale[] = {NOTE_B2, NOTE_CS3, NOTE_D3, NOTE_E3, NOTE_FS3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_CS4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_G4, NOTE_A4,NOTE_B4, NOTE_CS5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_G5, NOTE_A5,NOTE_B5, NOTE_CS6, NOTE_D6, NOTE_E6, NOTE_FS6, NOTE_G6, NOTE_A6,NOTE_B6, NOTE_CS7, NOTE_D7, NOTE_E7, NOTE_FS7, NOTE_G7, NOTE_A7,NOTE_B7, NOTE_CS8, NOTE_D8 };
+int timeSignatures[] = { 1, 2, 3, 4, 6, 8, 9, 12, 16 };
 
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
 float xAngle;
 float yAngle;
 float zAngle;
-float measurementsPerSecond = 20;
+float measurementsPerSecond = 50;
 float rotationalThreshold = .05;
 int delaySeconds = 0;
 int baseDelay = 1000;
@@ -73,7 +74,12 @@ void loop() {
         scaleAccessor = 37;
       }
       musicTone.play(scale[scaleAccessor], (yAngle));
-      delaySeconds = baseDelay / pow(2, floor(zAngle / 45));
+      
+      int scaleIndex = floor(zAngle / 45);
+      if (scaleIndex > 8) {
+        scaleIndex = 8;
+      }
+      delaySeconds = baseDelay / timeSignatures[scaleIndex];
     }
     else {
       delaySeconds -= (1000 / measurementsPerSecond);
