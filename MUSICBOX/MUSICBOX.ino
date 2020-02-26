5964aa90e134f02446714a35c70b907ded116c0f
Knar33 <adbyerly@comcast.net>
2020-02-26 00:16:18 -0600

    Smoothed out the sound on the musicbox and set new initial values

--- MUSICBOX/MUSICBOX.ino
+++ MUSICBOX/MUSICBOX.ino
@@ -9,9 +9,9 @@
 
 #define SD_ChipSelectPin 10
 #define speakerPin 9
-#define xModifier 2
+#define xModifier 1
 #define yModifier 1
-#define zModifier 1
+#define zModifier .5
 
 int scale[] = {NOTE_B2, NOTE_CS3, NOTE_D3, NOTE_E3, NOTE_FS3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_CS4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_G4, NOTE_A4,NOTE_B4, NOTE_CS5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_G5, NOTE_A5,NOTE_B5, NOTE_CS6, NOTE_D6, NOTE_E6, NOTE_FS6, NOTE_G6, NOTE_A6,NOTE_B6, NOTE_CS7, NOTE_D7, NOTE_E7, NOTE_FS7, NOTE_G7, NOTE_A7,NOTE_B7, NOTE_CS8, NOTE_D8 };
 
@@ -19,9 +19,10 @@ Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
 float xAngle;
 float yAngle;
 float zAngle;
-float measurementsPerSecond = 10;
+float measurementsPerSecond = 20;
 float rotationalThreshold = .05;
 int delaySeconds = 0;
+int baseDelay = 1000;
 TMRpcm tmrpcm;
 Tone musicTone;
 
@@ -32,7 +33,7 @@ void setup() {
   
   xAngle = 210;
   yAngle = 100;
-  zAngle = 100;
+  zAngle = 200;
   
   gyro.enableAutoRange(true);
   if(!gyro.begin(GYRO_RANGE_2000DPS))
@@ -55,8 +56,8 @@ void loop() {
     }
     if (checkRotationalThreshold(rotationalThreshold, event.gyro.y)) {
       yAngle += (event.gyro.y / measurementsPerSecond * 57.2958) * yModifier;
-      if (yAngle < 0) {
-        yAngle = 0;
+      if (yAngle < 50) {
+        yAngle = 50;
       }
     }
     if (checkRotationalThreshold(rotationalThreshold, event.gyro.z)) {
@@ -72,7 +73,7 @@ void loop() {
         scaleAccessor = 37;
       }
       musicTone.play(scale[scaleAccessor], (yAngle));
-      delaySeconds = (zAngle);
+      delaySeconds = baseDelay / pow(2, floor(zAngle / 45));
     }
     else {
       delaySeconds -= (1000 / measurementsPerSecond);
